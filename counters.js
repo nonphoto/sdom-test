a82042e44a16158c95d8c1aaa72143f9da67f065
Jonas Luebbers <jonaskluebbers@gmail.com>
2020-08-09 14:11:43 -0400

    Replace S with flyd

--- counters.js
+++ counters.js
@@ -1,26 +1,50 @@
-import S from "./lib/s.js";
-import { render, h, isStream } from "./lib/sdom.js";
-import range from "./lib/range.js";
+import S from "./lib/flyd.js";
+import { createElement } from "./lib/sdom.js";
 
-const t = S.data(0),
-  loop = (_t) => (t(_t), requestAnimationFrame(loop));
-
-const background = S(() => `rgb(${(t() * 0.01) % 255}, 100, 100)`);
-
-const counter = () => {
-  const count = S.data(0);
-  return h("button", { onclick: (e) => count(count() + 1) }, [count]);
+const M = {
+  pop: () => ({ type: "pop" }),
+  push: (component) => (...args) => ({
+    type: "push",
+    value: component(...args),
+  }),
 };
 
-const counters = S.data({ type: "none", value: null });
+const time = S.stream(0);
+const loop = (t) => {
+  time(t);
+  requestAnimationFrame(loop);
+};
 
-const main = h("div", { style: { background } }, [
-  h("button", { onclick: () => counters({ type: "pop" }) }, ["less"]),
-  h("button", { onclick: () => counters({ type: "push", value: counter() }) }, [
-    "more",
-  ]),
-  h("div", {}, counters),
-]);
+const background = S.map((t) => {
+  const v = (t * 0.01) % 255;
+  return `rgb(${v}, ${v}, ${v})`;
+}, time);
 
-document.body.appendChild(render(main));
+const counter = () => {
+  const onclick = S.stream();
+  const count = S.scan(
+    (a, b) => a + b,
+    0,
+    S.map(() => 1, onclick)
+  );
+  return ["button", { onclick }, count];
+};
+
+const main = () => {
+  const moreClick = S.stream();
+  const lessClick = S.stream();
+  const counters = S.merge(
+    S.map(M.pop, lessClick),
+    S.map(M.push(counter), moreClick)
+  );
+  return [
+    "div",
+    { style: { background } },
+    ["button", { onclick: lessClick }, "less"],
+    ["button", { onclick: moreClick }, "more"],
+    ["div", { mutator: counters }],
+  ];
+};
+
+document.body.appendChild(createElement(main()));
 loop();
